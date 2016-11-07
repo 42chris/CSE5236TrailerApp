09eec31fa3dbc9a50be64f4e03eeb1569829fa09
petrilla10 <petrilla.10@gmail.com>
2016-11-06 19:21:44 -0500

    Clip creation commit

--- app/src/main/java/edu/ohiostate/movietrailer/Clip.java
+++ app/src/main/java/edu/ohiostate/movietrailer/Clip.java
@@ -1,18 +1,30 @@
 package edu.ohiostate.movietrailer;
 
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.net.Uri;
 import android.util.Log;
-
 import com.googlecode.mp4parser.FileDataSourceImpl;
 import com.googlecode.mp4parser.authoring.Movie;
 import com.googlecode.mp4parser.authoring.Track;
 import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
 import com.googlecode.mp4parser.authoring.tracks.H263TrackImpl;
 import com.googlecode.mp4parser.authoring.tracks.h264.H264TrackImpl;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectStreamException;
+import java.io.Reader;
 import java.io.Serializable;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.URI;
 import java.util.ArrayList;
+import org.apache.commons.io.IOUtils;
 
 /**
  * Created by andrewpetrilla on 10/29/16.
@@ -20,22 +32,47 @@ import java.util.ArrayList;
 
 public class Clip implements Serializable{
 
-    private String clipID;
+    private String clipID = "CLIP";
     private String path;
     private boolean created;
     private Movie h264Track;
 
-    public Clip(String path){
+    public static final String PREFIX = "stream2file";
+    public static final String SUFFIX = ".mp4";
+
+    public Clip(String path, Context _context){
         this.path = path;
         this.created = true;
+        InputStream test1 =null;
+        File testFile = null;
+        Movie h264Track = null;
+        try{
+            test1 = _context.getAssets().open(path);
+        }catch(IOException e){
+            Log.d("Clip"+clipID,"File not Found. could not find asset");
+        }try {
+            testFile = stream2file(test1);
+        }catch (IOException e){
+            Log.d("Clip"+clipID,"Stream 2 file failed");
+        }
         try {
-            h264Track = MovieCreator.build(path);
+            this.h264Track = MovieCreator.build(new FileDataSourceImpl(testFile));
+            Log.d("Clip"+clipID,"*******************************CREATED H264 Track!*************************************\n\n\n*************************************\n\n");
         }catch(IOException e){
             Log.d("Clip"+clipID,"File not Found. video not added to Clip object");
         }
     }
 
-    public Clip(){
+    private File stream2file (InputStream in) throws IOException {
+        final File tempFile = File.createTempFile(PREFIX, SUFFIX);
+        tempFile.deleteOnExit();
+        try (FileOutputStream out = new FileOutputStream(tempFile)) {
+            IOUtils.copy(in, out);
+        }
+        return tempFile;
+    }
+
+    public Clip(Context _context){
         this.created = false;
     }
 
@@ -67,6 +104,7 @@ public class Clip implements Serializable{
         }
         out.writeBoolean(created);
         out.writeObject(h264Track);
+        /*out.writeObject(context);*/
         out.flush();
     }
 
@@ -85,5 +123,7 @@ public class Clip implements Serializable{
             throws ObjectStreamException
     {
     }
+
+
 }
 
