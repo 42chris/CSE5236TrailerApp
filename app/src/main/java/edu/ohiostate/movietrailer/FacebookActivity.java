4050d6a78f30947791c5339a378e5a264f75918b
petrilla10 <petrilla.10@gmail.com>
2016-11-14 11:54:29 -0500

    Media Player working

--- app/src/main/java/edu/ohiostate/movietrailer/FacebookActivity.java
+++ app/src/main/java/edu/ohiostate/movietrailer/FacebookActivity.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.Spinner;
+import android.widget.VideoView;
 
 import com.facebook.share.model.ShareVideo;
 import com.facebook.share.model.ShareVideoContent;
@@ -25,6 +27,9 @@ public class FacebookActivity extends Activity {
     ShareVideo share;
     ShareVideoContent content;
     Button shareButton;
+    private VideoView videoView;
+    private MediaController mediaController;
+    private int position = 0;
 
     private static final String TAG = "FacebookActivity";
 
@@ -35,17 +40,50 @@ public class FacebookActivity extends Activity {
 
         Intent mIntent = getIntent();
         String filePath = mIntent.getStringExtra("file");
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        if (mediaController == null) {
+            mediaController = new MediaController(FacebookActivity.this);
+
+            // Set the videoView that acts as the anchor for the MediaController.
+            mediaController.setAnchorView(videoView);
+
+
+            // Set MediaController for VideoView
+            videoView.setMediaController(mediaController);
+        }
         shareButton = (Button) findViewById(R.id.shareButton);
         Uri uri = Uri.fromFile(new File(filePath));
         share = new ShareVideo.Builder().setLocalUrl(uri).build();
         content = new ShareVideoContent.Builder().setVideo(share).build();
 
-        MediaPlayer mp = new MediaPlayer();
-        try{
-            mp.setDataSource(filePath);
-        }catch(IOException e){
-            Log.d(TAG, "Video failed to play");
-        }
+            videoView.setVideoPath(filePath);
+
+        videoView.requestFocus();
+
+        // When the video file ready for playback.
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+
+
+                videoView.seekTo(position);
+                if (position == 0) {
+                    videoView.start();
+                }
+
+                // When video Screen change size.
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+
+                        // Re-Set the videoView that acts as the anchor for the MediaController
+                        mediaController.setAnchorView(videoView);
+                    }
+                });
+            }
+        });
+
 
         shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +92,28 @@ public class FacebookActivity extends Activity {
             }
         });
 
+    }
+
+    // When you change direction of phone, this method will be called.
+    // It store the state of video (Current position)
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        // Store current position.
+        savedInstanceState.putInt("CurrentPosition", videoView.getCurrentPosition());
+        videoView.pause();
+    }
+
+
+    // After rotating the phone. This method is called.
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Get saved position.
+        position = savedInstanceState.getInt("CurrentPosition");
+        videoView.seekTo(position);
     }
 
     @Override
